80415aa2381dcac86b671aae0212783d95b4a20d
marijadev <marija.fo@gmail.com>
2018-05-11 14:41:10 +0200

    add working search

--- reportReact/reports-app/src/app/components/createReport/CreateReportPage.js
+++ reportReact/reports-app/src/app/components/createReport/CreateReportPage.js
@@ -1,49 +1,16 @@
 import React, { Component, Fragment } from 'react';
-import { Search } from '../../partials/Search';
-import { SingleCandidateCard } from './SingleCandidateCard';
-import { candidateService } from '../../../services/candidateService';
 import { Menu } from './Menu';
+import { SelectCandidate } from './SelectCandidate';
+
 class CreateReportPage extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			candidates: null,
-			selectedCandidates: {}
 		}
-	}
-
-	componentDidMount() { 
-		this.getReports();
-	 }
-
-	getReports = () => {
-		candidateService.fetchCandidates()
-			.then(candidates => {
-				this.setState({ candidates: candidates })
-			})
-		}
-		
-	filterCandidates = (inputField) => {
-		const { candidates } = this.state;
-		const filteredCandidates = candidates.filter(candidate => {
-			return candidate.name.toLowerCase().includes(inputField.toLowerCase())
-		})
-		this.setState({ filteredCandidates });
-	}
-
-	handleSelectCandidate = (candidate) => {
-		this.setState({ selectedCandidates: candidate })
 	}
 
 	render() {
 
-		const result = this.state.filteredCandidates ? this.state.filteredCandidates : this.state.candidates;
-		const { selectedCandidates: candidates } = this.state;
-			
-		if(this.state.candidates === null) {
-			return <h1>Loading...</h1>
-		}
-		
 		return (
 			<Fragment>
 				<div className="create-report-wrapper">
@@ -52,11 +19,8 @@ class CreateReportPage extends Component {
 					</div>
 					<div className="create-report-separator"></div>
 					<div className="switch-reports-wrapper">
-						<div className="search-holder">
-							<Search searchParams={this.filterCandidates} />
-						</div>
 						<div className="switch-reports-holder">
-							{result.map((candidate, index) => {return <SingleCandidateCard key={index} candidate={candidate}/>})}
+							<SelectCandidate />
 						</div>
 						<div className="create-report-navigation">
 							<div className="previous-btn create-report-btns">
---
+++ reportReact/reports-app/src/app/components/createReport/SelectCandidate.js
@@ -0,0 +1,56 @@
+import React, { Component, Fragment } from 'react';
+import { SingleCandidateCard } from './SingleCandidateCard';
+import { candidateService } from '../../../services/candidateService';
+import { Search } from '../../partials/Search';
+class SelectCandidate extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            candidates: [],
+            selectedCandidates: []
+        }
+    }
+
+    componentDidMount() {
+        console.log(this.getReports());
+    }
+
+    getReports = () => {
+        candidateService.fetchCandidates()
+            .then(candidates => {
+                this.setState({ candidates })
+            })
+    }
+
+    filterCandidates = (inputField) => {
+        const { candidates } = this.state;
+        const selectedCandidates = candidates.filter(candidate => {
+            return candidate.name.toLowerCase().includes(inputField.toLowerCase())
+        })
+        this.setState({ selectedCandidates });
+    }
+
+    handleSelectCandidate = (candidate) => {
+        this.setState({ selectedCandidates: candidate })
+    }
+
+    render() {
+        const result = this.state.selectedCandidates.length !== 0 ? this.state.selectedCandidates : this.state.candidates;
+        const { selectedCandidates: candidates } = this.state;
+
+        if (this.state.candidates.length === 0) {
+            return <h1>Loading...</h1>
+        }
+
+        return (
+            <Fragment>
+                <div className="search-holder">
+                    <Search searchParams={this.filterCandidates} />
+                </div>
+                {result.map((candidate, index) => <SingleCandidateCard key={index} candidate={candidate} handleSelectReport={this.handleSelectReport}/>)}
+            </Fragment>
+        )
+    }
+}
+
+export { SelectCandidate };
